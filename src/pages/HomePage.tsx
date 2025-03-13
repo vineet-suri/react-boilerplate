8c1e87886a18233d44f7f20781a647b8d555c5fa
Vineet Suri <vineetsuri@Vineets-MacBook-Air.local>
2025-03-13 15:21:05 +0530

    Add folder structure

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+const HomePage = () => {
+  return (
+    <div className="container">
+      <h1>Welcome to My React App 🚀</h1>
+      <p>This is the home page.</p>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,10 @@
+const NotFoundPage = () => {
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>Oops! The page you’re looking for doesn’t exist.</p>
+    </div>
+  );
+};
+
+export default NotFoundPage;
